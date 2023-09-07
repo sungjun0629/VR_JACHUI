57bb4041d3d550cd7428717cd33209e3b75109fd
Walkerworker <fghj160@naver.com>
2023-09-07 15:28:27 +0900

    Widget Add
    FurnitureCategory, FurnitureSelect, Main, RoomChoice

---
+++ Source/VR_JACHUI/Public/RoomChoiceWidget.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RoomChoiceWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VR_JACHUI_API URoomChoiceWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, meta = (BindWidget), Category = RoomChoice)
+	class UTextBlock* Text;
+
+};
