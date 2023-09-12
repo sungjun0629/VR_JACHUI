25c4be2f06923132b5c75a410fefa82800593f53
whi_co <113486696+sungjun0629@users.noreply.github.com>
2023-09-12 16:26:48 +0900

    Feat : Make List Furniture

--- Source/VR_JACHUI/Private/FurnitureList_Bed.cpp
+++ Source/VR_JACHUI/Private/FurnitureList_Bed.cpp
@@ -2,4 +2,21 @@
 
 
 #include "FurnitureList_Bed.h"
+#include "Components/TextBlock.h"
 
+
+
+void UFurnitureList_Bed::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	text_furniture->SetText(FText::FromString(TEXT("ho")));
+	UE_LOG(LogTemp, Warning, TEXT("entity starts"));
+}
+
+void UFurnitureList_Bed::changeText()
+{
+	UE_LOG(LogTemp,Warning,TEXT("before text : %s"), *(text_furniture->GetText()).ToString())
+	text_furniture->SetText(FText::FromString(TEXT("ho2")));
+	UE_LOG(LogTemp,Warning,TEXT("after text : %s"), *(text_furniture->GetText()).ToString())
+}
