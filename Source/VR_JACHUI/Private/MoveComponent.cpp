d1ce70004454db908485e33e13a99652159b0248
xowjd1 <taejeong311@gmail.com>
2023-09-14 19:19:29 +0900

    인풋 제대로 작동

--- Source/VR_JACHUI/Private/MoveComponent.cpp
+++ Source/VR_JACHUI/Private/MoveComponent.cpp
@@ -38,6 +38,8 @@ void UMoveComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 
 void UMoveComponent::SetupPlayerInputComponent(class UEnhancedInputComponent* enhancedInputComponent, TArray<class UInputAction*> inputActions)
 {
+	FString str = FString::Printf(TEXT("SetupPlayerInputComponent"));
+	GEngine->AddOnScreenDebugMessage(0, 10, FColor::Red, str);
 	enhancedInputComponent->BindAction(inputActions[0], ETriggerEvent::Triggered, this, &UMoveComponent::Move);
 	enhancedInputComponent->BindAction(inputActions[0], ETriggerEvent::Completed, this, &UMoveComponent::Move);
 	enhancedInputComponent->BindAction(inputActions[1], ETriggerEvent::Triggered, this, &UMoveComponent::Rotate);
@@ -48,6 +50,8 @@ void UMoveComponent::Move(const FInputActionValue& value)
 {
 	FVector2D controllerInput = value.Get<FVector2D>();
 	
+	FString str = FString::Printf(TEXT("x : %.2f, y : %.2f"), controllerInput.X, controllerInput.Y);
+	GEngine->AddOnScreenDebugMessage(0, 1, FColor::Red, str);
 
 	FVector forwardVec = FRotationMatrix(player->pc->GetControlRotation()).GetUnitAxis(EAxis::X);
 	FVector rightVec = FRotationMatrix(player->pc ->GetControlRotation()).GetUnitAxis(EAxis::Y);
