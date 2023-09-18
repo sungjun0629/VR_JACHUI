94088494bb6d510844bb2ec1ed7b7e56a1ce005b
xowjd1 <taejeong311@gmail.com>
2023-09-18 15:48:43 +0900

    프로토 발표전 일정부분 수정

--- Source/VR_JACHUI/Private/MoveComponent.cpp
+++ Source/VR_JACHUI/Private/MoveComponent.cpp
@@ -57,8 +57,8 @@ void UMoveComponent::Move(const FInputActionValue& value)
 {
 	FVector2D controllerInput = value.Get<FVector2D>();
 
-	FString str = FString::Printf(TEXT("x : %.2f, y : %.2f"), controllerInput.X, controllerInput.Y);
-	GEngine->AddOnScreenDebugMessage(0, 1, FColor::Red, str);
+	//FString str = FString::Printf(TEXT("x : %.2f, y : %.2f"), controllerInput.X, controllerInput.Y);
+	//GEngine->AddOnScreenDebugMessage(0, 1, FColor::Red, str);
 
 	FVector forwardVec = FRotationMatrix(player->pc->GetControlRotation()).GetUnitAxis(EAxis::X);
 	FVector rightVec = FRotationMatrix(player->pc->GetControlRotation()).GetUnitAxis(EAxis::Y);
