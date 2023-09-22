f7155484f4b775070362695329cd7fea4e28f599
whi_co <113486696+sungjun0629@users.noreply.github.com>
2023-09-22 17:21:31 +0900

    Fix : build Error

--- Source/VR_JACHUI/Private/WidgetPointerComponent.cpp
+++ Source/VR_JACHUI/Private/WidgetPointerComponent.cpp
@@ -1,4 +1,4 @@
-`// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 #include "WidgetPointerComponent.h"
 #include "EnhancedInputComponent.h"
 #include "VRCharacter.h"
